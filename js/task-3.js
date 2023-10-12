cb493ccb5a114a22f9dfe1c89bb0f9f5d55c3a12
serhiyserhiychuk <141840178+serhiyserhiychuk@users.noreply.github.com>
2023-10-12 12:24:25 +0300

    Correction

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,9 @@
 function getElementWidth(content, padding, border) {
-  return content + padding * 2 + border * 2;
+  return (
+    Number.parseFloat(content) +
+    Number.parseFloat(padding) * 2 +
+    Number.parseFloat(border) * 2
+  );
 }
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
